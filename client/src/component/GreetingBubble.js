af0acacbd08883d075f9b5c613d5ee2930973557
Tonislav Tachev <tachevv_@abv.bg>
2020-08-04 17:30:16 +0300

    Added the funcctionality to see live user count

--- client/src/component/GreetingBubble.js
+++ client/src/component/GreetingBubble.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const GreetingBubble = () => {
     return (
-        <div style={{padding:5, display:'flex', flexDirection:'row', width:220,height:20,marginLeft:35,marginTop:8,backgroundColor:'#ed2f65', borderRadius:10, color:'#FFF', justifyContent:'center'}}>
+        <div style={{padding:5, display:'flex', flexDirection:'row', width:220,height:28,marginLeft:35,marginTop:8,backgroundColor:'#ed2f65', borderRadius:10, color:'#FFF', justifyContent:'center'}}>
              <p style={{marginTop:-3}}>Welcome To ChatCord</p>
         </div>
     )
--- client/src/component/Livevisitors.js
+++ client/src/component/Livevisitors.js
@@ -1,19 +1,55 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { Table } from 'reactstrap';
+import socketIO from 'socket.io-client';
+import TableRoll from './TableRoll'
 const Livevisitors = () => {
+    const ENDPOINT = "http://127.0.0.1:3001"
+    var socket = socketIO(ENDPOINT);
+    const [react, setReact] = useState("");
+    const [angular, setAng] = useState("");
+    const [vue, setVue] = useState("");
+        useEffect(()=>{
+      socket.on('connect', ()=>{});
+        setInterval(() => {
+          socket.emit('getListOfUserInReact', {room1:'React'});
+          socket.emit('getListOfUserInAngular', {room1:'Angular'});
+          socket.emit('getListOfUserInVue', {room1:'Vue'});
+        }, 5000);
+     
+        socket.on("ListOfReact", (data)=>{
+          setReact(data);
+        })
+        socket.on("ListOfVue", (data)=>{
+          setVue(data);
+        })
+        socket.on("ListOfAngular", (data)=>{
+          setAng(data);
+        })
+    },[])
+
     return (
     <div className="container" style={{marginTop:20}}>
         <h5>Current visitors</h5>
         <Table>
       <thead>
         <tr>
-          <th>#</th>
-          <th>IP</th>
-          <th>City</th>
-          <th>Country</th>
+          <th>Room</th>
+          <th>People in room</th>
         </tr>
       </thead>
       <tbody>
+         <tr>
+             <td>React</td>
+            <td><b>{react}</b> users currently in room</td>
+          </tr>
+          <tr>
+             <td>Vue</td>
+             <td><b>{vue}</b> users currently in room</td>
+          </tr>
+          <tr>
+             <td>Angular</td>
+             <td><b>{angular}</b> users currently in room</td>
+          </tr>
       </tbody>
     </Table>
     </div>
